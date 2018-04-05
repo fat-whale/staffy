1d31ba220172694951803b966dce62e9155aa929
Urbanonymous <ugutitej@gmail.com>
2018-04-05 04:51:22 +0100

    rename the module, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "staffy",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Urbano Gutierrez",
     author_email = "urbano@fatwhale.io",
     description = ("A simple wrapper for our internal staff API."),
